7e9971ba2bb8f6ab47d34707dc20334c831ca821
Lindrew <spongebobrafael@gmail.com>
2023-10-05 17:36:23 +0800

    changed things

--- src/main.c
+++ src/main.c
@@ -2,11 +2,14 @@
 
 int main(void)
 {
-    printf("Welcome to BankOne!");
+    printf("Welcome to BankO!\n");
 
-    printf("Nate was here :)");
+    char name[32];
+
+    printf("Who dis?");
+    scanf("%s", name);
+    printf("%s was here :)", name);
     printf("Nate was here hours ago.");
     printf("lexa cutie");
     return 0;
-
 }
